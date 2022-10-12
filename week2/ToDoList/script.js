b3920a5487e87d5198fd1db04f09b54367bb6b01
[talkingOrange] <sonrisa-bonita@naver.com>
2022-10-12 22:34:17 +0900

    mission_1_변수 선언

---
+++ week2/ToDoList/script.js
@@ -0,0 +1,13 @@
+// 2022-10-11-PM 9:00 ~ AM 1:00
+// 2022-10-12-PM 10:00 ~ PM 11:00
+/////////////////////////////
+// 1. 오늘, 내일, 함께 보기 //
+/////////////////////////////
+
+//사용할 변수 선언
+const today = document.querySelector(".today");
+const tomorrow = document.querySelector(".tomorrow");
+const together = document.querySelector(".together");
+const rightSection = document.querySelector(".rSection");
+const leftSection = document.querySelector(".lSection");
+const clickedClass = "hidden";
