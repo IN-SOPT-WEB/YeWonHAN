c4d8f2c0b6b1f4a537cc2f9a96c5b33115d26487
[talkingOrange] <sonrisa-bonita@naver.com>
2022-11-12 01:42:04 +0900

    file setting

---
+++ week4/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import Search from "../components/Search";
+import User from "../components/User";
+import HistoryBlock from "../components/HistoryBlock";
+
+export default function App() {
+  return (
+    <>
+      <Search />
+      <HistoryBlock />
+      <User />
+    </>
+  );
+}
